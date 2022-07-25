07aa731ccfcd927bdf219c9b9f80b22505d3965e
Davut Özgür Süküti <80099012+davutozgursukuti4531@users.noreply.github.com>
2022-07-25 16:00:57 +0300

    Create commonjs.test.js

---
+++ test/commonjs.test.js
@@ -0,0 +1,12 @@
+const turkceSozlukApi = require("../index.js")
+
+
+
+async function kelimeDeneme() {
+    return console.log(await turkceSozlukApi.KelimeAnlamCekme("baklava"))
+}
+async function atasozu_deyimDeneme() {
+    return console.log(await turkceSozlukApi.AtasozuDeyimAnlamCekme("damlaya damlaya göl olur"))
+}
+kelimeDeneme()
+atasozu_deyimDeneme()
