bc74fb84220812f1da03b2ca8a9c0e097d7dacd4
Davut Özgür Süküti <80099012+davutozgursukuti4531@users.noreply.github.com>
2022-08-19 14:04:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const axios = require("axios").default
 const rexarTools = require("rexar-tools")
 const EventEmitter = require("node:events")
+const colorette = require("colorette")
+const versionControl = require("./Utils/versionControl.js")
 
 
 
-class Sozluk extends EventEmitter{
+class turkceSozlukApi extends EventEmitter{
     constructor(){
         super()
+        versionControl(colorette.red("=> ") + colorette.blue("Modülü sürümü eski güncellemek için terminale: npm i turkce-sozluk-api@latest yazın."))
     }
     /**
      * 
@@ -19,7 +22,10 @@ class Sozluk extends EventEmitter{
             url: "https://sozluk.gov.tr/gts?ara=" + encodeURI(kelime)
         })
         const body = response.data[0]
-        return {
+        const kelime_bulundumu = response.data.error ? false : true
+        return kelime_bulundumu === false ? {
+            kelime_bulundumu
+        } : {
             kelime,
             anlam: body?.anlamlarListe[0]?.anlam,
             ikinci_anlam: body?.anlamlarListe[1]?.anlam,
@@ -30,11 +36,12 @@ class Sozluk extends EventEmitter{
             cogul_mu: rexarTools.booleanify(rexarTools.numberify(body.cogul_mu)),
             ornek: body?.anlamlarListe[0]?.orneklerListe ? body?.anlamlarListe[0]?.orneklerListe[0] !== undefined ? body?.anlamlarListe[0]?.orneklerListe[0].ornek : undefined : undefined,
             ikinci_ornek: body?.anlamlarListe[1]?.orneklerListe ? body?.anlamlarListe[1]?.orneklerListe[0] !== undefined ? body?.anlamlarListe[1]?.orneklerListe[0].ornek : undefined : undefined,
-            birlesikler: body?.birlesikler,
-            atasozu_deyim: body?.atasozu ? body?.atasozu[0].madde : undefined
+            birlesikler: body?.birlesikler ? body.birlesikler : undefined,
+            atasozu_deyim: body?.atasozu ? body?.atasozu[0].madde : undefined,
+            kelime_bulundumu
         }
       } catch(error){
-            this.emit("KelimeAnlamCekmeHata", error)
+            this.emit("apiHata", error)
       }
     }
     /**
@@ -48,14 +55,16 @@ class Sozluk extends EventEmitter{
             url: "https://sozluk.gov.tr/atasozu?ara=" + encodeURI(inputSoz)
         })
         const body = response.data[0]
+        const soz_bulundumu = response.data.error ? false : true
         return {
             soz: body?.sozum,
             anlam: body?.anlami,
             anahtar_kelimeler: body?.anahtar,
-            atasozu_mu_deyim_mi: body?.turu2
+            atasozu_mu_deyim_mi: body?.turu2,
+            soz_bulundumu
         }
       } catch(error){
-        this.emit("AtasozuDeyimAnlamCekmeHata", error)
+        this.emit("apiHata", error)
       }
     }
     /**
@@ -67,8 +76,8 @@ class Sozluk extends EventEmitter{
         const response = await axios.request({
             url: "https://sozluk.gov.tr/gts?ara=" + encodeURI(kelime)
         })
-        const body = response.data[0]
-        return body.error === "Sonuç Bulunamadı" ? false : true
+        const body = response.data
+        return body.error ? false : true
     }
     /**
      * 
@@ -80,10 +89,10 @@ class Sozluk extends EventEmitter{
             url: "https://sozluk.gov.tr/atasozu?ara=" + encodeURI(atasozu)
         })
         const body = response.data[0]
-        return body.error === "Sonuç Bulunamadı" ? false : true
+        return body.error ? false : true
     }
 }
 
 
 
-module.exports = new Sozluk()
+module.exports = new turkceSozlukApi()
