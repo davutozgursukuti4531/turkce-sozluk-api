87a7d58ce9f1ccc1495a0ccc3c7526871a52a7a0
Davut Özgür Süküti <80099012+davutozgursukuti4531@users.noreply.github.com>
2022-09-01 23:44:41 +0300

    Update Returns.ts

--- Types/Returns.ts
+++ Types/Returns.ts
@@ -1,5 +1,6 @@
 export type KelimeAnlamCekmeReturns = {
-    kelime: string
+    kelime: string | undefined
+    id: string | undefined
     anlam: string | undefined
     ikinci_anlam: string | undefined
     ucuncu_anlam: string | undefined
@@ -12,18 +13,78 @@ export type KelimeAnlamCekmeReturns = {
     birlesikler: string | undefined
     kelime_bulundumu: boolean
     atasozu_deyim: string | undefined
-} | undefined
+}
 export type AtasozuDeyimAnlamCekmeReturns = {
     soz: string | undefined
+    id: string | undefined
     anlam: string | undefined
     anahtar_kelimeler: string | undefined
     atasozu_mu_deyim_mi: string | undefined
     soz_bulundumu: boolean
-} | undefined
+}
 export type IsımAnlamCekmeReturns = {
     ad: string | undefined
+    id: string | undefined
     anlam: string | undefined
     cinsiyeti: string | undefined
     isim_bulundumu: boolean
+}
+export type EczacilikTerimAnlamCekmeReturns = {
+    terim: string | undefined
+    id: string | undefined,
+    ingilizce: string | undefined
+    tanim: string | undefined
+    kontrol: string | undefined
+}
+export type KelimeLehcelerCekmeReturns = {
+    azerice: {
+        azerice_1: string | undefined,
+        azerice_2: string | undefined,
+        azerice_3: string | undefined,
+        azerice_4: string | undefined
+    },
+    kazakca: {
+        kazakca_1: string | undefined,
+        kazakca_2: string | undefined,
+        kazakca_3: string | undefined,
+        kazakca_4: string | undefined
+    },
+    turkmence: {
+        turkmence_1: string | undefined,
+        turkmence_2: string | undefined,
+        turkmence_3: string | undefined,
+        turkmence_4: string | undefined
+    },
+    baskurtca: {
+        baskurtca_1: string | undefined,
+        baskurtca_2: string | undefined,
+        baskurtca_3: string | undefined,
+        baskurtca_4: string | undefined
+    },
+    kirgizca: {
+        kirgizca_1: string | undefined,
+        kirgizca_2: string | undefined,
+        kirgizca_3: string | undefined,
+        kirgizca_4: string | undefined
+    },
+    ozbekce: {
+        ozbekce_1: string | undefined,
+        ozbekce_2: string | undefined,
+        ozbekce_3: string | undefined,
+        ozbekce_4: string | undefined
+    },
+    tatarca: {
+        tatarca_1: string | undefined,
+        tatarca_2: string | undefined,
+        tatarca_3: string | undefined,
+        tatarca_4: string | undefined
+    },
+    uygurca: {
+        uygurca_1: string | undefined,
+        uygurca_2: string | undefined,
+        uygurca_3: string | undefined,
+        uygurca_4: string | undefined
+    },
+    kelime_bulundumu: boolean
 }
 export type Events = "isimApiHata" | "atasozuDeyimApiHata" | "kelimeApiHata"
