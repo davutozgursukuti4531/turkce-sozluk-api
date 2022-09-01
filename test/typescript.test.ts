ffe389bc0476b5cb991bdb6172c8b74a55e341a3
Davut Özgür Süküti <80099012+davutozgursukuti4531@users.noreply.github.com>
2022-09-01 23:46:38 +0300

    Update typescript.test.ts

--- test/typescript.test.ts
+++ test/typescript.test.ts
@@ -2,4 +2,4 @@ import turkceSozlukApi from "turkce-sozluk-api";
 import { KelimeAnlamCekmeReturns, AtasozuDeyimAnlamCekmeReturns } from "../Types/Returns";
 
 
-turkceSozlukApi.KelimeAnlamCekme("sakla samanı").then((v: KelimeAnlamCekmeReturns) => console.log(v))
+turkceSozlukApi.KelimeAnlamCekme("baklava").then((v: KelimeAnlamCekmeReturns) => console.log(v))
